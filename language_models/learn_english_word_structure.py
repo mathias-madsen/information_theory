44e21c0ac68e40c40eaa806de6b2d4e4ae1ea5ab
mathias <mathias.winther@gmail.com>
2021-04-06 10:20:15 +0200

    be more explicit about the baseline in word-structure example

--- language_models/learn_english_word_structure.py
+++ language_models/learn_english_word_structure.py
@@ -56,44 +56,83 @@ def split_list(elements, min_chunk_size=1000):
 
 if __name__ == "__main__":
 
+    chunk_size = 100
+
     with open("long_text.txt", "r") as source:
         words = extract_words(source.read())
-    
-    grammar = create_random_grammar(20)
+        words = list(set(words))  # do not repeat frequent words
+        # words = [w for w in words if 5 <= len(w) <= 20]
 
-    for updateidx, chunk in enumerate(split_list(words, 1000)):
+    letters, counts = np.unique(list("".join(words)), return_counts=True)
+    counts += 100  # add virtual observations of each letter
+    freqs = counts / np.sum(counts)
+    freqdict = {ch: num / np.sum(counts) for ch, num in zip(letters, counts)}
+    baseline = lambda word: sum(-np.log2(freqdict[c]) for c in word)
 
-        print("--- Epoch %s ---" % (updateidx + 1))
+    chunks = split_list(words, chunk_size)
+    num_chunks = len(chunks)
+    chunks = [chunks[i] for i in np.random.permutation(num_chunks)]
 
-        # initialize accumulators with a few virtual observations:
-        sumtrans = 10.0 * grammar.transitions.copy()
-        sumemits = 10.0 * grammar.emissions.copy()
+    print("Loaded a data set of %s words.\n" % len(words))
 
-        losses = []
-        for sentence in tqdm(chunk, leave=False):
-            inner = grammar.compute_inside_probabilities(sentence)
-            outer = grammar.compute_outside_probabilities(inner, initial=0)
-            sumtrans += grammar.sum_transition_probabilities(inner, outer)
-            sumemits += grammar.sum_emission_probabilities(sentence, outer)
-            loglike = np.log(inner[0, -1, 0])
-            losses.append((-1) * loglike / len(sentence))
+    print("The data was split up into %s chunks of size %s.\n"
+          % (num_chunks, chunk_size))
 
-        print("Mean loss per character (before updating):")
-        print("%.3f ± %.3f bits.\n" % (np.mean(losses), np.std(losses)))
+    print("Example words from the data set:\n")
+    print(", ".join("%r" % w for w in np.random.choice(words, size=50)))
+    print("\n")
 
-        print("Some words sampled from the grammar (before updating):\n")
-        for _ in range(30):
-            pseudoword = "".join(grammar.sample_tree().terminals)
-            print("%r," % pseudoword, end=" ")
-        print(". . .\n")
+    grammar = create_random_grammar(80)
 
-        norms = np.sum(sumtrans, axis=(1, 2)) + np.sum(sumemits, axis=1)
-        sumtrans /= norms[:, None, None]
-        sumemits /= norms[:, None]
-        grammar.update_from_matrices(sumtrans, sumemits)
+    for epochidx in range(5):
 
-    print("Some samples from the estimated grammar:")
-    print()
-    for _ in range(10):
-        print("".join())
-    print()
+        grammar.save_as("wordgrammar.npz")
+
+        for updateidx, chunk in enumerate(chunks):
+
+            print("--- Epoch %s, step %s / %s ---\n" %
+                  (epochidx + 1, updateidx + 1, len(chunks)))
+
+            # initialize accumulators with a few virtual observations:
+            sumtrans = 5.0 * grammar.transitions.copy()
+            sumemits = 5.0 * grammar.emissions.copy()
+            sumemits += 1.0 * freqs
+
+            relative_losses = []
+            monogram_losses = []
+            losses = []
+
+            for sentence in tqdm(chunk, leave=False, unit="words"):
+                # compute the contributions to the parameter update:
+                inner = grammar.compute_inside_probabilities(sentence)
+                outer = grammar.compute_outside_probabilities(inner, initial=0)
+                sumtrans += grammar.sum_transition_probabilities(inner, outer)
+                sumemits += grammar.sum_emission_probabilities(sentence, outer)
+                # for reporting purposes, also compute various loss measures:
+                loss = (-1) * np.log(inner[0, -1, 0])  # in absolute terms
+                losses.append(loss / len(sentence))  # / uniform-model loss
+                monogram_loss = baseline(sentence)
+                monogram_losses.append(monogram_loss / len(sentence))
+                relative_losses.append(loss / monogram_loss)
+
+            stats = np.mean(losses), np.std(losses)
+            monostats = np.mean(monogram_losses), np.std(monogram_losses)
+            increasing = list(chunk[np.argsort(relative_losses)])
+
+            print("Model loss per on this batch: %.3f ± %.3f bits." % stats)
+            print("Baseline loss per on this batch: %.3f ± %.3f bits." % monostats)
+            print("Highest excessive loss: %r" % increasing[-5:])
+            print("Lowest excessive loss: %r" % increasing[:5])
+            print("")
+
+            norms = np.sum(sumtrans, axis=(1, 2)) + np.sum(sumemits, axis=1)
+            sumtrans /= norms[:, None, None]
+            sumemits /= norms[:, None]
+            grammar.update_from_matrices(sumtrans, sumemits)
+
+            print("Some words sampled from the grammar (after updating):\n")
+            for _ in range(30):
+                pseudoword = "".join(grammar.sample_tree().terminals)
+                print("%r," % pseudoword, end=" ")
+            print(". . .\n")
+            print("")
